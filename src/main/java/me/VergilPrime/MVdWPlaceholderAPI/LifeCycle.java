5455e21c2cb8fd2d1e9bed833ebbef60affcd243
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-12-04 17:27:13 -0800

    Trying to peice together something that works

---
+++ src/main/java/me/VergilPrime/MVdWPlaceholderAPI/LifeCycle.java
@@ -0,0 +1,31 @@
+import com.laytonsmith.PureUtilities.SimpleVersion;
+import com.laytonsmith.PureUtilities.Version;
+import com.laytonsmith.core.extensions.AbstractExtension;
+import com.laytonsmith.core.extensions.MSExtension;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@MSExtension("MVdWPlaceholderAPI")
+public class MyExtension extends AbstractExtension {
+    @Override
+    public Version getVersion() {
+        return new SimpleVersion(0,0,1);
+    }
+
+    @Override
+    public void onStartup() {
+
+      if(Bukkit.getPluginManager().isPluginEnabled("MVdWPlaceholderAPI")) {
+        System.out.println("CHMVdWPlaceholderAPI version " + getVersion() + " loaded.");
+      } else {
+        System.out.println("CHMVdWPlaceholderAPI version " + getVersion() + " not loaded.");
+        System.out.println("MVdWPlaceholderAPI not found.");
+      }
+    }
+
+    @Override
+    public void onShutdown() {
+      System.out.println("MVdWPlaceholderAPI version " + getVersion() + " unloaded.");
+    }
+}
